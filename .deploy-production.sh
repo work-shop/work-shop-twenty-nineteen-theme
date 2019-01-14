b72fc9f59b9044f206108e5d5cf230c880834e1f
Greg Nemes <mail@gregnemes.com>
2019-01-14 10:57:40 -0500

    added env and deploy files, added gitignore, removed bundles directory

---
+++ .deploy-production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#npm run build
+
+source ./.env
+
+# Uploads
+#scp -P $KINSTA_PORT -r ../../uploads $KINSTA_USER@$KINSTA_IP:./public/wp-content/
+
+# Custom Theme
+scp -P $KINSTA_PRODUCTION_PORT -r ../../themes/custom $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes
+
+# Bundles only
+#scp -P $KINSTA_PRODUCTION_PORT -r ./bundles $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom
+
+# Plugins and must use plugins
+#scp -P $KINSTA_PRODUCTION_PORT -r ../../plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
+#scp -P $KINSTA_PRODUCTION_PORT -r ../../mu-plugins $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/
+
+#specific plugins
+#scp -P $KINSTA_PRODUCTION_PORT -r ../../plugins/wc-product-customer-list-premium $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/plugins
+
+#specific files
+#scp -P $KINSTA_PRODUCTION_PORT ./functions/post-types/classes/class-nam-class.php $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom/functions/post-types/classes/
+
+#functions.php
+#scp -P $KINSTA_PRODUCTION_PORT ./functions.php $KINSTA_PRODUCTION_USER@$KINSTA_PRODUCTION_IP:./public/wp-content/themes/custom/
+
+
+curl -L https://$PRODUCTION_SITE_URL/kinsta-clear-cache-all/ 
